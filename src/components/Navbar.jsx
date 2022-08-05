a11c3b03239cb8efcf3c1d5e46ab5e35a74f68fe
Salman Hamza <90480347+salmanhamza@users.noreply.github.com>
2022-08-05 08:57:00 -0700

    5

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,14 @@
-import { AppBar, styled, Toolbar, Typography } from "@mui/material";
+import { CatchingPokemon, Mail, Notifications } from "@mui/icons-material";
+import {
+  AppBar,
+  Avatar,
+  Badge,
+  Box,
+  InputBase,
+  styled,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import React from "react";
 
 const ToolbarStyled = styled(Toolbar)({
@@ -6,11 +16,65 @@ const ToolbarStyled = styled(Toolbar)({
   justifyContent: "space-between",
 });
 
+const Search = styled("div")(({ theme }) => ({
+  backgroundColor: "white",
+  padding: "0 10px",
+  borderRadius: theme.shape.borderRadius,
+  width: "40%",
+}));
+// * for big screen more than 600px
+const Icon = styled(Box)(({ theme }) => ({
+  display: "none",
+  alignItems: "center",
+  gap: "20px",
+  // only show up if it is bigger than small
+  [theme.breakpoints.up("sm")]: {
+    display: "flex",
+  },
+}));
+
+//* for small screen more than 600px
+const SmallScreen = styled(Box)(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  gap: "10px",
+  [theme.breakpoints.up("sm")]: {
+    display: "none",
+  },
+}));
+
 const Navbar = () => {
   return (
     <AppBar position="sticky">
       <ToolbarStyled>
-        <Typography variant="h6">Social</Typography>
+        <Typography variant="h6" sx={{ display: { xs: "none", sm: "block" } }}>
+          Social
+        </Typography>
+        <CatchingPokemon sx={{ display: { xs: "block", sm: "none" } }} />
+        <Search>
+          <InputBase placeholder="Search..." />
+        </Search>
+        <Icon>
+          <Badge badgeContent={4} color="error">
+            <Mail color="action" sx={{ color: "white" }} />
+          </Badge>
+          <Badge badgeContent={2} color="error">
+            <Notifications color="action" sx={{ color: "white" }} />
+          </Badge>
+          <Avatar
+            sx={{ width: 30, height: 30 }}
+            alt="Salamn"
+            src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?cs=srgb&dl=pexels-pixabay-220453.jpg&fm=jpg"
+          />
+          <SmallScreen>
+            <Avatar
+              sx={{ width: 30, height: 30 }}
+              alt="Salamn"
+              src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?cs=srgb&dl=pexels-pixabay-220453.jpg&fm=jpg"
+            />
+            <Typography variant="span">Salman</Typography>
+          </SmallScreen>
+        </Icon>
       </ToolbarStyled>
     </AppBar>
   );
