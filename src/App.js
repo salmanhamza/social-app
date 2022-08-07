059c91496425c9416e25bc4cee60af9db1dbf073
Salman Hamza <90480347+salmanhamza@users.noreply.github.com>
2022-08-06 22:53:14 -0700

    5

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import Feed from "./components/Feed";
 import Rightbar from "./components/Rightbar";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
+import { Stack } from "@mui/material";
 
 function App() {
   return (
     <div>
       <Navbar />
-      <Sidebar />
-      <Feed />
-      <Rightbar />
+      <Stack direction="row" spacing={2} justifyContent="space-between">
+        <Sidebar />
+        <Feed />
+        <Rightbar />
+      </Stack>
     </div>
   );
 }
