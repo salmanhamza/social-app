6fb7ad2ff7ece6f3c5c4d0156d91a16fc9471f0c
Salman Hamza <90480347+salmanhamza@users.noreply.github.com>
2022-08-07 00:06:33 -0700

    5

---
+++ src/components/Add.jsx
@@ -0,0 +1,24 @@
+import { Box, Fab, Tooltip } from "@mui/material";
+import { Add as AddIcon } from "@mui/icons-material";
+import React from "react";
+
+const Add = () => {
+  return (
+    <Box>
+      <Tooltip
+        title="delete"
+        sx={{
+          position: "fixed",
+          bottom: 20,
+          left: { xs: "calc(50%-25px)", md: 30 },
+        }}
+      >
+        <Fab color="primary" aria-label="add">
+          <AddIcon />
+        </Fab>
+      </Tooltip>
+    </Box>
+  );
+};
+
+export default Add;
